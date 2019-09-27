72295e6495f877af3cc555946cc55770ed65079f
kamil-kolenda <55441342+kamil-kolenda@users.noreply.github.com>
2019-09-28 01:22:15 +0200

    Add previousWorkingDay to calamari service (#10)
    * Add previousWorkingDay to calamari service
    
    * CR fixes

--- src/calamari/calamari.service.ts
+++ src/calamari/calamari.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, HttpService, Injectable } from '@nestjs/common';
 import moment from 'moment';
+import { ConfigService } from 'nestjs-config';
 
 import DateTimeUtils from '../utils/DateTimeUtils';
 
@@ -7,10 +8,14 @@ import { TimeSheetEntryAuthor } from './interfaces/TimeSheetEntryAuthor.interfac
 import { TimeSheetEntry } from './interfaces/TimeSheetEntry.interface';
 import { TimeSheetEntriesInput } from './interfaces/TimeSheetEntriesInput';
 import { Person } from './interfaces/Person.interface';
+import { HolidayItem } from './interfaces/HolidayItem.interface';
 
 @Injectable()
 export class CalamariService {
-  constructor(private readonly http: HttpService) {}
+  constructor(
+    private readonly http: HttpService,
+    private readonly config: ConfigService,
+  ) { }
 
   private getPersonObject(author: TimeSheetEntryAuthor): Person {
     return {
@@ -18,6 +23,24 @@ export class CalamariService {
       lastName: author.lastName,
       email: author.email,
     };
+  }
+
+  private async holidays({ from, to }: TimeSheetEntriesInput): Promise<HolidayItem[]> {
+    return this.http
+      .post('holiday/v1/find', { from, to, employee: this.config.get('calamari.employee') })
+      .toPromise()
+      .then(result => result.data)
+      .catch(err => {
+        throw new BadRequestException('calamari thrown error', err.message);
+      });
+  }
+
+  private async isHoliday(date: Date): Promise<Boolean> {
+    const dateString = DateTimeUtils.getDateString(date);
+
+    const holidays = await this.holidays({ from: dateString, to: dateString });
+
+    return holidays.length > 0;
   }
 
   private async timesheetEntries({ from, to }: TimeSheetEntriesInput): Promise<TimeSheetEntry[]> {
@@ -44,5 +67,17 @@ export class CalamariService {
 
       return [...presentPeople, this.getPersonObject(timeSheetEntry.person)];
     }, []);
+  }
+
+  public async previousWorkingDay(date: Date): Promise<Date> {
+    const previousBussinessDay = DateTimeUtils.getPreviousBusinessDay(date);
+
+    const isHoliday = this.isHoliday(previousBussinessDay);
+
+    if (isHoliday) {
+      return this.previousWorkingDay(previousBussinessDay);
+    }
+
+    return previousBussinessDay;
   }
 }
