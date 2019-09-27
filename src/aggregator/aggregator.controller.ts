a398b1722335dd00707e1f2cd00322b4d0b9c6fd
Szymon <szymon.bartczak@rst-it.com>
2019-09-28 00:18:36 +0200

    Added google authentication to aggregator endpoint

--- src/aggregator/aggregator.controller.ts
+++ src/aggregator/aggregator.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller, Get, Query, UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AggregatorService } from './aggregator.service';
 import { UserWorklogResult } from './interfaces/user-worklog-result.interface';
@@ -7,6 +10,7 @@ import { UserWorklogResult } from './interfaces/user-worklog-result.interface';
 export class AggregatorController {
   constructor(private readonly aggregatorService: AggregatorService) {}
 
+  @UseGuards(AuthGuard('google-token'))
   @Get('worklogs')
   public async aggregate(@Query() date: Date): Promise<UserWorklogResult[]> {
     return this.aggregatorService.aggregate(date);
