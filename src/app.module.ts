1a35d62c23698e35892ae5e6c2a7bdde43049872
kamil-kolenda <55441342+kamil-kolenda@users.noreply.github.com>
2019-09-27 20:07:41 +0200

    Add tempo module (#3)
    * Add tempo module
    
    * Fix eslint
    
    * CR fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import * as path from 'path';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
 
 import { AppController } from './app.controller';
-import { ConfigModule } from 'nestjs-config';
+import { TempoModule } from './tempo/tempo.module';
 
 @Module({
   imports: [
     ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    TempoModule,
   ],
   controllers: [AppController],
   providers: [],
